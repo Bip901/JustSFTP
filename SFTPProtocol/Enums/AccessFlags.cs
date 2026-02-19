99023d8c8805f0a74146a8675cb9915fb4c3fdb0
Bip901 <74911767+Bip901@users.noreply.github.com>
2026-02-19 23:07:52 +0200

    Document access flags

--- SFTPProtocol/Enums/AccessFlags.cs
+++ SFTPProtocol/Enums/AccessFlags.cs
@@ -3,23 +3,53 @@ using System.IO;
 
 namespace JustSFTP.Protocol.Enums;
 
+/// <summary>
+/// pflags
+/// </summary>
 [Flags]
 public enum AccessFlags : uint
 {
+    /// <summary>
+    /// SSH_FXF_READ
+    /// </summary>
     Read = 0x01,
+    /// <summary>
+    /// SSH_FXF_WRITE
+    /// </summary>
     Write = 0x02,
+    /// <summary>
+    /// SSH_FXF_APPEND
+    /// </summary>
     Append = 0x04,
+    /// <summary>
+    /// SSH_FXF_CREAT
+    /// </summary>
     Create = 0x08,
+    /// <summary>
+    /// SSH_FXF_TRUNC
+    /// </summary>
     Truncate = 0x10,
+    /// <summary>
+    /// SSH_FXF_EXCL
+    /// </summary>
     Exclusive = 0x20,
+    /// <summary>
+    /// SSH_FXF_TEXT
+    /// </summary>
     Text = 0x40,
 }
 
+/// <summary>
+/// Helper methods for <see cref="AccessFlags"/>.
+/// </summary>
 public static class AccessFlagsExtensionMethods
 {
+    /// <summary>
+    /// Returns the <see cref="FileMode"/> flags that best represent the given access flags.
+    /// </summary>
     public static FileMode ToFileMode(this AccessFlags flags)
     {
-        var filemode = FileMode.Open;
+        FileMode filemode = FileMode.Open;
         if (flags.HasFlag(AccessFlags.Append))
         {
             filemode = FileMode.Append;
@@ -43,24 +73,15 @@ public static class AccessFlagsExtensionMethods
         return filemode;
     }
 
+    /// <summary>
+    /// Returns the <see cref="FileAccess"/> flags that best represent the given access flags.
+    /// </summary>
     public static FileAccess ToFileAccess(this AccessFlags flags)
     {
-        var fileAccess = FileAccess.Read;
-        if (flags.HasFlag(AccessFlags.Read) && flags.HasFlag(AccessFlags.Write))
+        FileAccess fileAccess = FileAccess.Read;
+        if (flags.HasFlag(AccessFlags.Write))
         {
-            fileAccess = FileAccess.ReadWrite;
-        }
-        else if (flags.HasFlag(AccessFlags.Read))
-        {
-            fileAccess = FileAccess.Read;
-        }
-        else if (flags.HasFlag(AccessFlags.Write))
-        {
-            fileAccess = FileAccess.Write;
-        }
-        else if (flags.HasFlag(AccessFlags.Text))
-        {
-            throw new NotImplementedException();
+            fileAccess |= FileAccess.Write;
         }
         return fileAccess;
     }
